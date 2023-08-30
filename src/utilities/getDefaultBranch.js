fbf167b3adedd9074139ac85ecc28599e357f563
Scheffczyk, Francesco <francesco.scheffczyk@hs-mainz.de>
2023-08-30 13:51:33 +0200

    Download file route

---
+++ src/utilities/getDefaultBranch.js
@@ -0,0 +1,27 @@
+import { BASE_URL } from "../variables.js";
+import handleResponseError from "./handleResponseError.js";
+
+async function getDefaultBranch(id, env, res) {
+	// Construct the repo endpoint URL
+	const REPO_ENDPOINT_URL = `${BASE_URL}/api/v4/projects/${id}/repository`;
+	const headers = { "Private-Token": env.GITLAB_ACCESS_TOKEN };
+
+	// Construct the branches endpoint URL
+	const BRANCH_ENDPOINT_URL = `${REPO_ENDPOINT_URL}/branches`;
+
+	// Fetch branches
+	const branchesResponse = await fetch(BRANCH_ENDPOINT_URL, { headers });
+
+	if (!branchesResponse.ok) {
+		handleResponseError(res, branchesResponse);
+	}
+
+	const branches = await branchesResponse.json();
+
+	// Filter default branch
+	const defaultBranch = branches.filter((branch) => branch.default);
+
+	return defaultBranch;
+}
+
+export { getDefaultBranch };
---
+++ src/variables.js
@@ -0,0 +1,4 @@
+const BASE_URL = "https://gitlab.rlp.net";
+const GROUP = "21057"; // equals to KITeGG on RLP GitLab
+
+export { BASE_URL, GROUP };
